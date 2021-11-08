70110e2996c60196769f58fccd7387176182846e
Ahmed <ahmedreda152@gmail.com>
2021-11-08 20:00:28 +0200

    add javascript functionality for menu

--- jsscripts.js
+++ jsscripts.js
@@ -1,5 +1,13 @@
+const menuIcon = document.querySelector('.burgerIcon');
+const menuContainer = document.querySelector('.menu-container');
+const closeButton = document.querySelector('.closeIcon');
 
-function change(){
-    const menu = document.querySelector("#burger");
-    menu.classList.add("check")
-}
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('isActive');
+  menuContainer.classList.toggle('isActive');
+});
+
+closeButton.addEventListener('click', () => {
+  menuIcon.classList.remove('isActive');
+  menuContainer.classList.remove('isActive');
+});
