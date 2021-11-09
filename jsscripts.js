7668c4a199706f8fa5a70658bb0a69e10074183a
charlesgobina <charles.gobina@gmail.com>
2021-11-09 17:31:08 +0100

    Implement functionality for sidenav

--- jsscripts.js
+++ jsscripts.js
@@ -1,13 +1,43 @@
 const menuIcon = document.querySelector('.burgerIcon');
 const menuContainer = document.querySelector('.menu-container');
 const closeButton = document.querySelector('.closeIcon');
+const body = document.querySelector('.body');
+const main = document.querySelector('#main');
+const navs = document.querySelectorAll('.menu-container .side-link');
 
 menuIcon.addEventListener('click', () => {
-  menuIcon.classList.toggle('isActive');
   menuContainer.classList.toggle('isActive');
+  body.style.filter = 'blur(8px)';
+  main.style.filter = 'blur(8px)';
 });
 
 closeButton.addEventListener('click', () => {
-  menuIcon.classList.remove('isActive');
   menuContainer.classList.remove('isActive');
+  body.style.filter = 'none';
+  main.style.filter = 'none';
+});
+
+navs.forEach((navItem) => {
+  if (navItem.getAttribute('href') === '#portfolio') {
+    navItem.addEventListener('click', () => {
+      menuContainer.classList.remove('isActive');
+      body.style.filter = 'none';
+      main.style.filter = 'none';
+    });
+  } else if (navItem.getAttribute('href') === '#aboutme') {
+    navItem.addEventListener('click', () => {
+      menuContainer.classList.remove('isActive');
+      body.style.filter = 'none';
+      main.style.filter = 'none';
+    });
+  } else if (navItem.getAttribute('href') === '#contact') {
+    navItem.addEventListener('click', () => {
+      menuContainer.classList.remove('isActive');
+      body.style.filter = 'none';
+      main.style.filter = 'none';
+    });
+  } else {
+    return navItem;
+  }
+  return null;
 });
